bfd475038154e6ed835f320ce245364ccc9b10f7
Iván_Casales <casalesivan@hotmail.com>
2022-04-19 20:36:06 -0300

    Ultima modificación 2.0

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,6 @@ namespace PII_Game_Of_Life
     {
         static void Main(string[] args)
         {
-
             bool[,] gameBoard = LeerArchivo.LeerArchivoFuncion(@"../../assets/board.txt");
             ImprimirTablero.ImprimirTableroFuncion(gameBoard);
         }
